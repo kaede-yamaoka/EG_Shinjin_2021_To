d5ec3cf0d3ae61fa7029fc03fcaa4a97b96ce931
kaede-yamaoka <yamaoka@e-geid.co.jp>
2021-04-28 11:56:02 +0900

    バリデーション / ログインフォーム作成

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,17 @@
 class Member < ApplicationRecord
+    validates :number, presence: true,
+        numericality: {
+            only_integer: true,
+            greater_than: 0,
+            less_than: 1000,
+            allow_blank: true
+        },
+        uniqueness: true
+    
+    validates :name, presence: true,
+        length: {minimum: 2, maximum: 20, allow_blank: true },
+        uniqueness: { case_sensitive: false }
+
     class << self
         def search(query)
             rel = order("number")
