0868da40ed0101cc20b97d5716048787a69a6828
sonoyamajouji <sonoyama@e-grid.co.jp>
2021-05-15 16:46:15 +0900

    members_controller編集

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,30 +1,51 @@
 class MembersController < ApplicationController
-    def index
-        @members = Member.order("number")
+    def index #一覧画面出力
+        @members = Member.order("number")#numberの降順に並び変える
     end
 
-    def show
+    def show #ユーザーの詳細画面
+       @member = Member.find(params[:id])
+
     end
 
     def new
+       @member = Member.new(birthday: Date.new(1900,5,4))
     end
 
     def edit
+       @member = Member.find(params[:id])
     end
 
     # 新規登録
     def create
+        @member = Member.new(params[:member])
+        if @member.save
+            redirect_to @member
+        else
+            render 'new'
+        end
     end
 
     # 更新
     def update
+        @member = Member.find(params[:id])
+        @member.assign_attributes(params[:member])
+        if @member.save
+            redirect_to @member
+        else
+            render 'edit'
+        end
     end
-
     # 削除
     def destroy
+        @member = Member.find(params[:id])
+        @member.destroy
+        redirect_to @member
     end
 
     # 検索機能
-    def search
+    def search 
+        @members = Member.search(params[:q])
+        render 'index'
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -22,5 +22,4 @@ ActiveRecord::Schema.define(version: 2021_04_27_062426) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 end
